e5898141ae454b6b0e18628154114f489fa3ac50
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-05-15 17:16:22 -0500

    💭

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# rag_assistant
+# Swarmauri Rag Assistant
+
+## Overview
+The Swarmauri Rag Assistant
+
+## Installation
+```bash
+pip install rag_assistant==0.1.1 --user
+```
+
+## Execution
+```bash
+! rag_assistant --api_key "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" --db_path "E:\\swarmauri_github/prompt_responses.db"
+```
--- rag_assistant/__init__.py
+++ rag_assistant/__init__.py
@@ -4,4 +4,14 @@ __long_desc__ = """# Swarmauri Rag Assistant
 
 ## Overview
 The Swarmauri Rag Assistant
+
+## Installation
+```bash
+pip install rag_assistant==0.1.1 --user
+```
+
+## Execution
+```bash
+! rag_assistant --api_key "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" --db_path "E:\\swarmauri_github/prompt_responses.db"
+```
 """
