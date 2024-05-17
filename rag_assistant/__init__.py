5e24e5e79f8bc86f601095633e301269ea2bebc5
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-05-16 20:54:31 -0500

    🚯Sanitize  - rm api_key

--- rag_assistant/__init__.py
+++ rag_assistant/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __short_desc__ = """The Swarmauri Rag Assistant is part of the swarmaURI framework."""
 __long_desc__ = """# Swarmauri Rag Assistant
 
@@ -12,6 +12,6 @@ pip install rag_assistant==0.1.1 --user
 
 ## Execution
 ```bash
-! rag_assistant --api_key "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" --db_path "E:\\swarmauri_github/prompt_responses.db"
+! rag_assistant --api_key "YOUR API KEY" --db_path "E:\\swarmauri_github/prompt_responses.db"
 ```
 """
