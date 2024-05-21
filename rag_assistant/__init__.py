8ee6593a564aa87ea58edfb22c46192f7dcc6cb7
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-05-21 02:47:17 -0500

    test

--- rag_assistant/__init__.py
+++ rag_assistant/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.10"
+__version__ = "0.1.12"
 __short_desc__ = """The Swarmauri Rag Assistant is part of the swarmaURI framework."""
 __long_desc__ = """# Swarmauri Rag Assistant
 
@@ -7,7 +7,7 @@ The Swarmauri Rag Assistant
 
 ## Installation
 ```bash
-pip install rag_assistant==0.1.10 --user
+pip install rag_assistant==0.1.12 --user
 ```
 
 ## Execution
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     },
     include_package_data=True,
     install_requires=['gradio',
-        'swarmauri[full]==0.2.3'],
+        'swarmauri[full]==0.2.5'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.10'
